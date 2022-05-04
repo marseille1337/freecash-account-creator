841672aac132e271aa03186e0ec2de78d6a4c1bb
marseille <104928491+marseille1337@users.noreply.github.com>
2022-05-04 20:12:55 +0200

    Add files via upload

---
+++ creator.js
@@ -0,0 +1,95 @@
+const { createCursor } = require('ghost-cursor-playwright')
+const cluster = require('cluster')
+const axios = require('axios')
+const { firefox } = require('playwright')
+const prompt = require('prompt-sync')({ sigint: true })
+fs = require('fs');
+
+var created = 0;
+var logger = fs.createWriteStream('accounts.txt', {
+    flags: 'a'
+  })
+
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+process.title = 'Ghost | Accounts Created: '+created;
+
+intro = 
+` 
+## ##   ###  ##   ## ##    ## ##   #### ##  
+##   ##   ##  ##  ##   ##  ##   ##  # ## ##  
+##        ##  ##  ##   ##  ####       ##     
+##  ###   ## ###  ##   ##   #####     ##     
+##   ##   ##  ##  ##   ##      ###    ##     
+##   ##   ##  ##  ##   ##  ##   ##    ##     
+ ## ##   ###  ##   ## ##    ## ##    ####    
+                                             
+FreeCash.com - marseille1337 & leakage1337
+
+`
+
+;(async () => {
+    if(cluster.isMaster) {
+        console.clear()
+        console.log('\x1b[35m%s\x1b[0m', (intro));
+        var threads = parseInt(prompt('\x1b[35mGhost | Threads -> \x1b[0m'))
+        for (var i = 0; i < threads; i++) {
+            cluster.fork()
+        }
+    } else {
+        await free_cash()
+    }
+
+})()
+
+async function free_cash() {
+    const browser = await firefox.launch({ headless: true })
+    var page = await browser.newPage()
+    const cursor = await createCursor(page)
+
+    await page.goto('https://freecash.com/')
+      
+    var email = 'ghost' + Math.random().toString(4).substring(2, 12);
+    await axios.post("https://api.internal.temp-mail.io/api/v3/email/new", {"domain": "kjkszpjcompany.com", "name": email});
+    email += "@kjkszpjcompany.com";
+    console.log('\x1b[35m%s\x1b[0m', 'Ghost | Got mail -> ' + email)
+    await page.bringToFront()
+    await page.type('input[id*=email]', email)
+    await page.type('input[id*=password]', "GhostOnTop!")
+    password = "GhostOnTop!";
+    console.log('\x1b[35m%s\x1b[0m', 'Ghost | Password -> ' + password)
+    try {
+        await page.$eval('input[type*=checkbox]', el => el.click())            
+    } catch (error) {}
+    const elements = await page.$$('//*[contains (text(), "Konto erstellen")]');
+    await sleep(1000)
+    var x = JSON.parse(JSON.stringify(await elements[3].boundingBox()))['x']
+    var y = JSON.parse(JSON.stringify(await elements[3].boundingBox()))['y']
+    var cords = {
+        x: x,
+        y: y,
+        width: 30,
+        height: 30
+    }
+    await cursor.actions.move(cords)
+    await cursor.actions.click(cords)
+
+    var emailData;
+    while (true) {
+        var emailData = await axios.get("https://api.internal.temp-mail.io/api/v3/email/" + email + "/messages").then(res => res.data)
+        if (emailData.length !== 0) {
+            var first = emailData[0].body_text.toString().split("*Verify Email Address ( ")[1]
+            emailData = first.split(" )*")[0]
+            break
+        }
+        await sleep(200)
+    }
+    console.log('\x1b[35m%s\x1b[0m', 'Ghost | Mail verified!')
+    await page.goto(emailData)
+    await page.bringToFront()
+    logger.write('\n' + email + ':' + password)
+    console.log('\x1b[35m%s\x1b[0m', 'Ghost | Account have been succesfully saved! -> accounts.txt')
+    process.send({ created: "+1" })
+}
